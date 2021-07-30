714dc56a7efe981479bc143332639fc56c8c7e27
Rintz01 <82634482+Rintz01@users.noreply.github.com>
2021-07-30 14:09:36 +0900

    Update app.py

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -2,5 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Docker!'
+def hell0_flask():
+    return "Hello, Flask!"
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
